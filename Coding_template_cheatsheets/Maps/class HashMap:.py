c96b180204ab32aa17e4e60919a7a9b8e228ef5b
SaraAu91 <cianciotta.sara@hmail.com>
2022-07-20 08:50:03 +1000

    Moved folder and files arrangements

---
+++ Coding_template_cheatsheets/Maps/class HashMap:.py
@@ -0,0 +1,14 @@
+class HashMap:
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for item in range(array_size)]
+
+    def hash(self, key):
+        self.key = key
+        key_bytes = key.encode()
+        #.encode() is a string method that converts a string into its corresponding bytes, a list-like object with the numerical representation of each character in the string.
+        hash_code = sum(key_bytes)
+        return hash_code
+    
+    def compressor(self, hash_code):
+        return hash_code % self.array_size
