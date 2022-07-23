19fec6fad97fdaca20e38ebf0a5a4a06641e7d6a
SaraAu91 <cianciotta.sara@hmail.com>
2022-07-23 11:37:15 +1000

    flower changed into item

--- Coding_template_cheatsheets/Maps/blossom_project /script.py
+++ Coding_template_cheatsheets/Maps/blossom_project /script.py
@@ -2,7 +2,7 @@ from class_HashMap_LinkedList_Chaining import HashMap
 from blossom_lib import flower_definitions
 
 blossom = HashMap(len(flower_definitions))
-for flower in flower_definitions:
-  blossom.assign(flower[0], flower[1])
+for item in flower_definitions:
+  blossom.assign(item[0], item[1])
 
 print(blossom.retrieve('daisy'))
